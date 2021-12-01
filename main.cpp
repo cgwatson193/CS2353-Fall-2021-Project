c5a0cc6e858979a0d848d9f2efb8c19df1b4e1fa
cgwatson193 <78247585+cgwatson193@users.noreply.github.com>
2021-12-01 09:22:26 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+using namespace std;
+bool xorop(bitset<8> test, int i){
+    if(i==test.size()-1)
+        return test[i];
+    return test[i]^xorop(test,i+1);
+}
+int main() {
+    string myString;
+    cout<<"\n    ----DEMO OF A BASIC INTEGRITY CHECK FUNCTION USED TO CHECKSUM----        ";
+    cout << "\n\n     Enter in a string to see if it has an odd number of 1 bits:\n";
+    getline (cin,myString);
+
+    bool result= false;
+    int count = 0;
+    // convert string to bits
+    cout<<"\n--"<<myString<<" CONVERTERTED TO BITS (8 per character)--\n";
+    for (std::size_t i = 0; i < myString.size(); ++i){
+
+        bitset<8> bits = bitset<8>(myString[i]);
+        cout<<"           char "<<i+1<<": "<<bits<<endl;
+        result = result != xorop(bits,0);
+        // find if string has even or odd # of 1's
+        for(int j =0;j<bits.size();j++){
+            if(bits[j]==1)
+                count++;
+        }
+    }
+
+    cout<<"\n      ----Boolean logic result----         \n"<<"  "<<result;
+    if(result==1)
+        cout<<" (True, string has odd number of 1 bits)\n";
+    if(result==0)
+        cout<<" (False, string has even number of 1 bits)\n";
+
+
+    cout<<"\n\n      ----VERIFICATION CHECK----         \n         Total number of 1's: "<<count<<endl;
+    int check = count%2;
+    cout<<"              "<< count <<" mod 2="<<result<<endl;
+    if(check==result)
+        cout<<"             CHECK PASSED\n";
+
+    else
+        cout<<"             CHECK FAILED\n";
+
+
+
+    return 0;
+}
